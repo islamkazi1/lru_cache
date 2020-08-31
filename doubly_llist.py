709d003af2000e71b51b898cd3f917bd8fe5ce4b
islamkazi1 <islamkazi1@gmail.com>
2020-08-31 16:17:39 -0400

    Add files via upload

--- doubly_llist.py
+++ doubly_llist.py
@@ -16,6 +16,11 @@ class DoublyLlist(object):
         self.tail = None
        
     def add_node(self, node):
+        '''
+        Adds node as the head and tail, if list is empty otherwise adds it after the head,
+        links the existing head as the previous of the node. Finally makes the node current head
+        :param node:
+        '''
         if self.head is None:
             self.head = node
             self.tail = node
@@ -23,11 +28,12 @@ class DoublyLlist(object):
             node.prev = self.head
             self.head.next = node
             self.head = node
-            
-        
         
     def remove_node(self, node):
-
+        '''
+        Removes the node from the list and adjusts the previous and next links accordingly
+        :param node:
+        '''
         if self.tail == node:
             self.tail = node.next
             self.tail.prev = None
